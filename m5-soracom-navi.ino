991f9aa5310ec4f60a9a5bda5a7fa79780f0b40a
toshiaki MATSUYAMA <toshiaki@localhost.localdomain>
2021-01-24 13:56:53 +0900

    download to SD and drawPngFile

--- m5-soracom-navi.ino
+++ m5-soracom-navi.ino
@@ -1,17 +1,21 @@
-#define LGFX_AUTODETECT
+//#define LGFX_AUTODETECT
 #include <M5Stack.h>
 #define TINY_GSM_MODEM_UBLOX
 #include <TinyGsmClient.h>
 #include<string.h>
 
-#include<LovyanGFX.hpp>
+#include<SD.h>
+
+//#include<LovyanGFX.hpp>
 
 #include <math.h>
 
 TinyGsm modem(Serial2); /* 3G board modem */
 TinyGsmClientSecure ctx(modem);
 
-static LGFX lcd;
+File f;
+
+//static LGFX lcd;
 //static LGFX_Sprite sprite(&lcd);
 
 int zoom = 14;
@@ -25,21 +29,22 @@ int vsat, usat;
 int year, month, day, hour, minute, second;
 
 void setup() {
-  M5.begin(false);
-  lcd.init();
+  M5.begin();
+  SD.begin();
+  //lcd.init();
   //  lcd.setRotation(1);
-  lcd.setBrightness(128);
+ // M5.Lcd.setBrightness(128);
 
   Serial.begin(115200);
 
 
-  lcd.print(F("modem.restart()"));
+  M5.Lcd.print(F("modem.restart()"));
   Serial2.begin(115200, SERIAL_8N1, 16, 17);
   modem.restart();
 
-  lcd.print(F("getModemInfo:"));
+  M5.Lcd.print(F("getModemInfo:"));
   String modemInfo = modem.getModemInfo();
-  lcd.println(modemInfo);
+  M5.Lcd.println(modemInfo);
 
   while (!modem.waitForNetwork()) {
     Serial.print(".");
@@ -51,13 +56,13 @@ void setup() {
     Serial.print(".");
   }
 
-  lcd.println(F("enable GPS"));
+  M5.Lcd.println(F("enable GPS"));
   modem.enableGPS();
 
   Serial.println(modem.localIP());
 
   delay(1000);
-  lcd.clear();
+  M5.Lcd.clear();
 }
 
 
@@ -70,13 +75,17 @@ void loop() {
   lon=138.0;
   speed=0;
   Serial.printf("%f,%f  speed=%f\n", lat, lon, speed);
-  lcd.setCursor(0, 0);
-  lcd.setTextSize(2);
-  lcd.printf("%3.3f%3.3f\n", lat, lon);
+  M5.Lcd.setCursor(0, 0);
+  M5.Lcd.setTextSize(2);
+  M5.Lcd.printf("%3.3f%3.3f\n", lat, lon);
 
   x = pow(2, 13) * (180 + lon) / 180;
   y = pow(2, 13) * (1 - log(tan(lat * 3.14 / 180) + 1 / cos(lat * 3.14 / 180)) / 3.14);
+  String dir = "xzy/std/"+String(zoom)+"/"+String(int(x))+"/";
+  String file=String(int(y))+".png";
 
+  Serial.println(dir);
+  
   path = String("GET /xyz/std/");
   path += String(zoom) + "/";
   path += String(int(x)) + "/";
@@ -84,7 +93,7 @@ void loop() {
   path += " HTTP/1.0";
   Serial.println(path.c_str());
 
-  //M5.Lcd.printf("XYZ=%d,%d/14",int(pow(2,13)*(180+lon)/180),int(pow(2,13)*(1-log(tan(lat*3.14/180)+1/cos(lat*3.14/180))/3.14)));
+  //M5.M5.Lcd.printf("XYZ=%d,%d/14",int(pow(2,13)*(180+lon)/180),int(pow(2,13)*(1-log(tan(lat*3.14/180)+1/cos(lat*3.14/180))/3.14)));
 
   if (!ctx.connect("cyberjapandata.gsi.go.jp", 443)) {
     Serial.println(F("Connect failed."));
@@ -109,11 +118,22 @@ void loop() {
   }
   char* p=_buf;
   Serial.println("body");
+  uint32_t readLen=0;
+  String openfile = "/"+dir+file;
+  f=SD.open("test.png",FILE_APPEND);
+  SD.mkdir(dir.c_str());
   while(ctx.connected()){
-    String line=ctx.readStringUntil('\n');
-    Serial.println(line);
-    strcat(_buf,line.c_str());
+    //String line=ctx.readStringUntil('\n');
+    char c=ctx.read();
+    
+   // Serial.println(c);
+    _buf[readLen]=c;
+    f.print(c);
+    readLen++;
+    //strcat(_buf,line.c_str());
   }
+
+  f.close();
   
   Serial.print("size of _buf: ");
   Serial.println(sizeof(_buf));
@@ -122,15 +142,17 @@ void loop() {
   
   ctx.stop();
 
-  //Serial.print("buf: ");
-  //Serial.println(_buf);
+  Serial.print("buf: ");
+  Serial.println(_buf);
 
   size_t _buf_s = strlen(_buf);
   Serial.print("buf_s:");
   Serial.println(_buf_s);
   
-  lcd.clear();
-  lcd.drawPng((const unsigned char *)_buf, _buf_s, 0, 0);
+  M5.Lcd.clear();
+  
+  Serial.println(openfile);
+  M5.Lcd.drawPngFile(SD,"test.png",0,0);
    _buf[0] = '\0';
   Serial.println("done");
   while(1){
