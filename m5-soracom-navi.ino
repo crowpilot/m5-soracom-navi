72d4b23dd03e7bff9cc55cf921faf12fa98ecc69
toshiaki MATSUYAMA <toshiaki@localhost.localdomain>
2021-01-30 19:15:26 +0900

    tile download fix

--- m5-soracom-navi.ino
+++ m5-soracom-navi.ino
@@ -71,7 +71,7 @@ void setup() {
   }
 
   lcd.println(F("modem done"));
-  
+
 
   Serial.println(modem.localIP());
 
@@ -84,14 +84,14 @@ void loop() {
   //M5.update();
   //WIFI location
 
-  http.getLocation(lat,lon);
+  http.getLocation(lat, lon);
 
   if (!lat) {
     Serial.println("modem gps");
     modem.enableGPS();
     modem.getGPS(&lat, &lon, &speed, &alt, &vsat, &usat, &accuracy, &year, &month, &day, &hour, &minute, &second);
-    zoom=10;
-    
+    zoom = 10;
+
   }
 
   Serial.printf("%f,%f  speed=%f\n accuracy=", lat, lon, speed);
@@ -106,76 +106,78 @@ void loop() {
   Serial.println(main.path().c_str());
   //http.getMap(main.path(), main.filename());
 
-  for (int x=-1;x<=1;x++){
-    for(int y=-1;y<=1;y++){
-      if(0<(main.offsetX()+256*(x+1)) or (main.offsetX()+256*x)<320){
-        if(0<(main.offsetY()+256*(y+1))or (main.offsetY()+256*y)<240){
-          http.getMap(main.path(x,y),main.filename(x,y));
+  for (int x = -1; x <= 1; x++) {
+    for (int y = -1; y <= 1; y++) {
+      if (0 < (main.offsetX() + 256 * (x + 1)) or (main.offsetX() + 256 * x) < 320) {
+        if (0 < (main.offsetY() + 256 * (y + 1)) or (main.offsetY() + 256 * y) < 240) {
+          http.getMap(main.path(x, y), main.filename(x, y));
         }
       }
     }
   }
-  
+
   lcd.clear();
 
   Serial.println(main.filename());
   //lcd.drawPngFile(SD, main.filename().c_str(), main.offsetX(), main.offsetY());
-  for (int x=-1;x<=1;x++){
-    for(int y=-1;y<=1;y++){
-      if(0<(main.offsetX()+256*(x+1)) or (main.offsetX()+256*x)<320){
-        if(0<(main.offsetY()+256*(y+1))or (main.offsetY()+256*y)<240){
-          sprite.createSprite(256,256);
-          sprite.drawPngFile(SD, main.filename(x,y).c_str(), 0, 0);
-          sprite.pushSprite(main.offsetX(x),main.offsetY(y));
+  for (int x = -1; x <= 1; x++) {
+    for (int y = -1; y <= 1; y++) {
+      if (0 < (main.offsetX() + 256 * (x + 1)) or (main.offsetX() + 256 * x) < 320) {
+        if (0 < (main.offsetY() + 256 * (y + 1)) or (main.offsetY() + 256 * y) < 240) {
+          sprite.createSprite(256, 256);
+          sprite.drawPngFile(SD, main.filename(x, y).c_str(), 0, 0);
+          sprite.pushSprite(main.offsetX(x), main.offsetY(y));
           //lcd.drawPngFile(SD, main.filename(x,y).c_str(), main.offsetX(x), main.offsetY(y));
         }
       }
     }
   }
   //   _buf[0] = '\0';
-  lcd.drawCircle(160,120,10,TFT_RED);
-  lcd.drawCircle(160,120,9,TFT_RED);
-  lcd.drawCircle(160,120,8,TFT_RED);
+  lcd.drawCircle(160, 120, 10, TFT_RED);
+  lcd.drawCircle(160, 120, 9, TFT_RED);
+  lcd.drawCircle(160, 120, 8, TFT_RED);
   Serial.println("done");
-  int loadx=-2;
-  int loady=-2;
+  int loadx = -2;
+  int loady = -2;
   while (1) {
     M5.update();
     //download sarani surround tile
-    if(M5.BtnA.wasPressed()){
+    if (M5.BtnA.wasPressed()) {
       zoom--;
       break;
     }
-    if(M5.BtnC.wasPressed()){
+    if (M5.BtnC.wasPressed()) {
       zoom++;
       break;
     }
     //http.getMap(main.path(loadx,loady),main.filename(loadx,loady));
-    http.getLocation(lat,lon);
-    main.setPlot(lat,lon);
-    lcd.drawCircle(main.plotX(),main.plotY(),7,TFT_RED);
-    if(main.plotX()<40 or main.plotX()>280){
+    http.getLocation(lat, lon);
+    main.setPlot(lat, lon);
+    lcd.drawCircle(main.plotX(), main.plotY(), 7, TFT_RED);
+    if (main.plotX()<40 or main.plotX()>280) {
       break;
     }
-    if(main.plotY()<40 or main.plotY()>200){
+    if (main.plotY()<40 or main.plotY()>200) {
       break;
     }
     Serial.println("wait 30 sec");
-    delay(30*1000);
-    if(http.getMap(main.path(loadx,loady),main.filename(loadx,loady))){
-      Serial.println("download surround map");
+    delay(30 * 1000);
+    if (loady < 2) {
+      if (http.getMap(main.path(loadx, loady), main.filename(loadx, loady))) {
+        Serial.println("download surround map");
+      } else {
+        Serial.println("wait 30 no download");
+        delay(30 * 1000);
+      }
+      Serial.printf("load tile %d,%d", loadx, loady);
+      loadx++;
+      if (loadx == 2) {
+        loady++;
+        loadx = -2;
+      }
     }else{
-      Serial.println("wait 30");
+      Serial.println("surround map ended");
       delay(30*1000);
-    }
-    Serial.printf("load tile %d,%d",loadx,loady);
-    loadx++;
-    if(loadx==2){
-      loady++;
-      loadx=-2;
-    }
-    if(loady==2){
-      loadx=2;
     }
   }
 }
